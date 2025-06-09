576d067abde1b762cd409c32a2e7307277d0e84d
Al Polinar <alpolinar@gmail.com>
2025-06-08 19:17:29 -0600

    defining modules and scope

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,50 @@
+mod front_of_the_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
+    }
+}
+
+pub fn eat_at_restaurant() {
+    let mut meal = back_of_the_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please.", meal.toast);
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal.
+    // meal.seasonal_fruit = String::from("blueberries");
+    let order1 = back_of_the_house::Appetizer::Soup;
+    let order2 = back_of_the_house::Appetizer::Salad;
+}
+
+fn deliver_order() {}
+
+mod back_of_the_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
